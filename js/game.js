00b82f6c5bb9b7bddaf9723ae3e808ff069ebce9
Kevin <kevin.ramos.873@my.csun.edu>
2022-03-28 15:04:30 -0700

    Add UI to Rock,Paper,Scissor

--- js/game.js
+++ js/game.js
@@ -1,4 +1,29 @@
+//get values
+
+const paper= document.getElementById('paper')
+//console.log(paper.id)
+const rock= document.getElementById('rock')
+const scissor= document.getElementById('scissor')
+const leftOp = document.querySelector('.leftOp')
+const OGSTATE = document.body
+const clone = OGSTATE.cloneNode(true)
+let click;
+const EMOJIS ={
+  paper :  "0x270B",
+  rock:"0x1FAA8",
+  scissor:"0x2702",
+}
+//
+
+
+
+//add eventlisterners
+
+
+
 //use a map to identify the winning scenarios for the pool;
+
+
 const options = {
   rock: "scissor",
   scissor: "paper",
@@ -80,4 +105,95 @@ function game() {
   }
 }
 
-console.log(game());
+//add event listeners
+
+
+leftOp.addEventListener('click',handleEvents
+,{once:true})
+
+function handleEvents(e){
+  console.log(e.target)
+  switch (e.target.id) {
+    case "rock":
+      creator(e)  
+       
+      break;
+    case "paper":
+      creator(e)
+      break;
+    case "scissor":
+      creator(e)
+      break;
+    default:
+  }
+
+}
+
+function creator(e){
+  console.log(e.target.id)
+  console.log(letsPlay(e.target.id,computerPlay()))
+  document.getElementById(e.target.id).remove()
+  let cp = computerPlay()
+  let computer = document.createElement('div')
+  let newPaper = document.createElement('div')
+ let fight= document.querySelector('.fight')
+ let versus = document.createElement('div')
+ let winner =document.createElement('div')
+ let body = document.querySelector('body')
+ let chooser;
+ let button = document.createElement('button')
+  //adding properties
+  newPaper.className='weapon'
+  newPaper.textContent= String.fromCodePoint(EMOJIS[e.target.id])
+  newPaper.id=e.target.id
+  computer.className="weapon"
+  computer.textContent=String.fromCodePoint(EMOJIS[cp])
+  versus.className="weapon"
+  versus.textContent=String.fromCodePoint("0x1F19A")
+  winner.style="color:white"
+  winner.className="winner"
+  button.textContent="Play Again?"
+  
+  //showing winner
+  chooser = letsPlay(e.target.id,cp)
+  if(chooser===1){
+    winner.textContent="You Won!"
+  } 
+  if(chooser===2){
+    winner.textContent="Computer Won " + String.fromCodePoint(EMOJIS[cp]) + 
+    " beats " + String.fromCodePoint(EMOJIS[e.target.id]) 
+  }
+  if(chooser===0){
+    winner.textContent="WHAT!? IT WAS TIE!!"
+  }
+
+  //appending to parent Node
+  fight.appendChild(newPaper)
+  fight.appendChild(versus)
+  fight.appendChild(computer)
+  body.appendChild(winner)
+  winner.appendChild(button)
+
+   button.onclick=('click', ()=>{
+    console.log("clicled")
+    newPaper.remove()
+    versus.remove()
+    computer.remove()
+    winner.remove()
+    if(e.target.id==='rock'){
+      document.querySelector('.leftOp').insertBefore(e.target,paper)
+    }
+    if(e.target.id==='paper'){
+      document.querySelector('.leftOp').insertBefore(e.target,scissor)
+    }
+    if(e.target.id==='scissor'){
+      document.querySelector('.leftOp').insertBefore(e.target,paper.nextSibling)
+
+    }
+    //document.body.replaceWith(clone)
+    click=true
+    leftOp.addEventListener('click',handleEvents,{once:true})
+  })
+  
+  console.log("skip")
+}
